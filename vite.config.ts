f557a0c2c729a6e90299c413f5824e25ffaa3e8b
Chris Foy <christopher.foy@momentumsolar.com>
2026-02-22 16:22:26 -0600

    feat: deployment

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,7 @@ import { tanstackStart } from '@tanstack/react-start/plugin/vite'
 import viteReact from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 import { nitro } from 'nitro/vite'
+import netlify from '@netlify/vite-plugin-tanstack-start'
 
 const config = defineConfig({
   plugins: [
@@ -16,6 +17,7 @@ const config = defineConfig({
     tailwindcss(),
     tanstackStart(),
     viteReact(),
+    netlify(),
   ],
 })
 
