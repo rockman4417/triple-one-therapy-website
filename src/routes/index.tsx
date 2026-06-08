840a4dba854ebb3ef54cd20326e5a98e80630be2
Chris Foy <christopher.foy@momentumsolar.com>
2026-06-08 09:38:20 -0500

    feat: add sections

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -107,7 +107,8 @@ function App() {
           >
             <SimplePracticeBookWidget
               label="Book a Consultation"
-              className="rounded-full bg-stone-900 px-6 py-3 text-sm font-semibold text-stone-50 transition hover:bg-stone-700"
+              // className="bg-stone-900 px-6 py-3 text-sm font-semibold text-stone-50 transition hover:bg-stone-700"
+              className="mt-16 inline-flex items-center justify-center border-[3px] border-[#ede8d1] px-8 py-3 text-2xl uppercase tracking-[0.28em] text-[#ede8d1] no-underline transition hover:bg-[#ede8d1] hover:text-[#433b2f] sm:px-14"
             />
           </div>
         </div>
